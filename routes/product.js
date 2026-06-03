29eeca662fde8ec343499543feb7b17fa7d55ae1
rwandaxcode <rwandaxcode@gmail.com>
2026-06-03 02:24:48 -0700

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const Product = require("../models/Product");
 
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.json(product);
+});
+
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+module.exports = router;
