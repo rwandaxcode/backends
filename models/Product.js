fccc323b1397ea53d743c1d11838372116e06941
rwandaxcode <rwandaxcode@gmail.com>
2026-06-03 02:20:37 -0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const productSchema = new mongoose.Schema({
+  productCode: String,
+  productName: String,
+  quantitySold: Number,
+  unitPrice: Number
+});
+
+module.exports = mongoose.model("Product", productSchema);
